effde468ec23fbfe66f4bc3b1f2ebb8ab727e01d
HugoOtth <hugo.otth@gmail.com>
2025-12-09 13:43:51 -0500

    remove partners

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,6 @@ export default function Home() {
     <main className="min-h-screen">
       <Header />
       <HeroSection />
-      <PartnerBanks />
       <WhatIsReverseMortgage />
       <BenefitsSection />
       <Calculator />
